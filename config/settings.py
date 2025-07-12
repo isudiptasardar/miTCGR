5d1a4ef62f7f661ac38d721ff7106fe9c62c3516
isudiptasardar <sudiptasbd@bicpu.edu.in>
2025-07-12 09:58:36 +0900

    win: model will be tested on vit for miraw dataset

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,10 @@
 from typing import Dict
 
 CONFIG: Dict[str, any] = {
-    'raw_data_path': 'data/deepmirtar.csv',
-    'm_rna_col_name': 'Target Site',
-    'mi_rna_col_name': 'miRNA_seq',
-    'class_col_name': 'label',
+    'raw_data_path': 'data/miraw.csv',
+    'm_rna_col_name': 'mRNA_Site_Transcript',
+    'mi_rna_col_name': 'mature_miRNA_Transcript',
+    'class_col_name': 'validation',
     'k_mer': 6,
     'batch_size': 32,
     'save_dir': 'output',
